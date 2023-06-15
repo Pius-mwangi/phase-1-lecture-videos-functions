4a058c3c4c35452d3e40bbd8d28e69de89a400e3
PIUS <relimoringa@gmail.com>
2023-06-15 12:28:46 +0300

    practice

---
+++ practice;.js
@@ -0,0 +1,10 @@
+name = "Pius";
+age = 23;
+height = '5,10';
+
+
+let names = (name, age, height) => {
+    return `hello ${name}, i think your age ${age}, is fine and height ${height} is perfect`
+
+}
+names(name,age,height);
